34669d3ab89d05aadb82a4423ba9d80c51fd2853
Marcio Pancheri <mjpancheri@gmail.com>
2021-09-19 11:15:16 -0300

    feat: add partial to render comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,17 @@ module ApplicationHelper
     menu_principal.html_safe
   end
 
+  def comentarios(comentavel)
+    comentarios = "<div id='comentarios'>"
+    comentarios << "<h3>Comentarios</h3>"
+    comentarios << render(:partial => "comentarios/comentario",
+                          :collection => comentavel.comentarios)
+    comentarios << "</div>"
+    comentarios << render(:partial => "comentarios/novo_comentario",
+                          :local => {:comentavel => comentavel})
+    comentarios.html_safe
+  end
+
   def valor_formatado(number)
     number_to_currency(number, :unit => "R$", :separator => ",", :delimiter => ".")
   end
